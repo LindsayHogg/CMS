869ea71269243ec52c9215274c2a01e523d94aa7
LindsayHogg <lindsayhogg@gmail.com>
2015-07-26 22:44:20 -0400

    More validation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
+  before_filter :authenticate_user!, only: [:new, :create]
+
   # GET /posts
   # GET /posts.json
   def index
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
 	
+	validates :content, presence: true,
+						length: { minimum: 250 }
+
 end
